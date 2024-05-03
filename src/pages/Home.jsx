95ab7fa4ac11acfc02ef00cd34f2034f5c18ae56
Pratik Suthar <d365technical@dynamicdaytechnologies.com>
2024-05-03 10:47:27 +0530

    Add: solution component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
 import AboutContainer from "../components/About/About-container";
+import SolutionContainer from "../components/Solutions/SolutionContainer";
 const Home = () => {
 	return (
 		<div>
 			<Navbar />
 			<Hero />
 			<AboutContainer />
+			<SolutionContainer />
 		</div>
 	);
 };
