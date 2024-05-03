44272ba06a9ffdf305850f6b9e6119875ea6cfd2
Pratik Suthar <d365technical@dynamicdaytechnologies.com>
2024-05-03 10:42:44 +0530

    Add: about fragment for home creen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
-
+import AboutContainer from "../components/About/About-container";
 const Home = () => {
 	return (
 		<div>
 			<Navbar />
 			<Hero />
+			<AboutContainer />
 		</div>
 	);
 };
